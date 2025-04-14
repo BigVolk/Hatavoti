e9d69f219e58d86b3184ba7b3d15133e3364ca44
Gabriel Bialik <gab2005345@gmail.com>
2025-04-14 10:28:08 +0300

    We have a map!

--- Hatavoti/src/Pages/FrontPage/FrontPage.jsx
+++ Hatavoti/src/Pages/FrontPage/FrontPage.jsx
@@ -1,14 +1,20 @@
+import { MapContainer, TileLayer,  } from 'react-leaflet'
+import "leaflet/dist/leaflet.css"
 import { Box } from "@mui/material"
 import BottomNav from '../../Components/BottomNav/BottomNav.jsx'
 import "./FrontPage.css"
 
 const FrontPage = () => {
 
-    return(
+    return (
         <>
             <Box className="front-page">
                 <Box className="map">
-                    Map
+                    <MapContainer center={[31.9510, 34.8881]} zoom={8} scrollWheelZoom={true}>
+                        <TileLayer
+                            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                        />
+                    </MapContainer>
                 </Box>
             </Box>
             <BottomNav />
