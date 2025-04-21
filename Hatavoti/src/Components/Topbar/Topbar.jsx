dd5bf151b0e4d2eb58464a4307122e3af00caf0f
Gabriel Bialik <gab2005345@gmail.com>
2025-04-21 16:57:30 +0300

    Top bar is better

--- Hatavoti/src/Components/Topbar/Topbar.jsx
+++ Hatavoti/src/Components/Topbar/Topbar.jsx
@@ -1,29 +1,40 @@
 import { Box, InputAdornment, TextField } from "@mui/material"
 import SearchIcon from '@mui/icons-material/Search';
 import hatavotiLogo from '../../assets/hatavoti-black.png'
+import { useState } from "react";
 import "./Topbar.css"
 
 const Topbar = () => {
+
+    const [searchText, setSearchText] = useState("")
 
     return (
         <>
             <Box className="around-top-bar">
                 <Box className="top-bar">
                     <TextField
-                        onSelect={() => console.log("selected")}
-                        onEnded={() => console.log("abort")}
-                        color="primary"
+                        onChange={(element) => console.log(element.target.value)}
+                        onClick={() => console.log("clicked")}
+                        type="text"
                         className="text-field"
                         placeholder={"חיפוש"}
                         variant="standard"
                         sx={{
+                            display: 'flex',
+                            justifyContent: 'center',
+                            direction: 'rtl',
                             backgroundColor: "#808000",
                             padding: '0 15px',
+                            height: '50px',
+                            "& .MuiInputBase-input": {
+                                fontSize: '20px',
+                                color: 'white'
+                            }
                         }}
                         slotProps={{
                             input: {
-                                startAdornment: <InputAdornment position="start"><SearchIcon /></InputAdornment>,
-                                endAdornment: <InputAdornment position="end"><img className="hatavoti-logo" src={hatavotiLogo} /></InputAdornment>
+                                endAdornment: <InputAdornment position="start"><SearchIcon /></InputAdornment>,
+                                startAdornment: <InputAdornment position="end"><img className="hatavoti-logo" src={hatavotiLogo} /></InputAdornment>
                             }
                         }}
                     />
