ea0ea9647a3651f80ffeb7bbb0232888fefedb0d
Gabriel Bialik <gab2005345@gmail.com>
2025-05-03 14:01:17 +0300

    Added pill options

--- Hatavoti/src/Components/Topbar/Topbar.jsx
+++ Hatavoti/src/Components/Topbar/Topbar.jsx
@@ -1,18 +1,72 @@
-import { Box, InputAdornment, TextField } from "@mui/material"
+import { Box, Button, InputAdornment, TextField } from "@mui/material"
 import SearchIcon from '@mui/icons-material/Search';
 import hatavotiLogo from '../../assets/hatavoti-black.png'
 import { useState } from "react";
+import RestaurantIcon from '@mui/icons-material/Restaurant';
+import CheckroomIcon from '@mui/icons-material/Checkroom';
+import LunchDiningIcon from '@mui/icons-material/LunchDining';
+import TheaterComedyIcon from '@mui/icons-material/TheaterComedy';
+import FitnessCenterIcon from '@mui/icons-material/FitnessCenter';
 import "./Topbar.css"
+
 
 const Topbar = () => {
 
+    const pillOptions = [
+        {
+            title: 'מסעדה',
+            icon: <RestaurantIcon />
+        },
+        {
+            title: 'בגדים',
+            icon: <CheckroomIcon />
+        },
+        {
+            title: 'המבורגר',
+            icon: <LunchDiningIcon />
+        },
+        {
+            title: 'בידור',
+            icon: <TheaterComedyIcon />
+        },
+        {
+            title: 'כושר',
+            icon: <FitnessCenterIcon />
+        },
+        {
+            title: 'מסעדה',
+            icon: <RestaurantIcon />
+        },
+        {
+            title: 'בגדים',
+            icon: <CheckroomIcon />
+        },
+        {
+            title: 'המבורגר',
+            icon: <LunchDiningIcon />
+        },
+        {
+            title: 'בידור',
+            icon: <TheaterComedyIcon />
+        },
+        {
+            title: 'כושר',
+            icon: <FitnessCenterIcon />
+        }
+    ]
     const [searchText, setSearchText] = useState("")
     const [isFullScreen, setIsFullScreen] = useState(false)
+
+    if (isFullScreen) {
+        document.addEventListener('backbutton', () => {
+            setIsFullScreen(!isFullScreen)
+        })
+    }
 
     return (
         <>
             <Box className="around-top-bar"
-                sx= {isFullScreen ? 
+                sx={isFullScreen ?
                     {
                         height: '100vh',
                         zIndex: 9999,
@@ -24,9 +78,9 @@ const Topbar = () => {
                     }
                 }
             >
-                <Box className="top-bar">
+                {/* <Box className="top-bar">
                     <TextField
-                        onChange={(element) => console.log(element.target.value)}
+                        onChange={(element) => setSearchText(element.target.value)}
                         onClick={() => setIsFullScreen(!isFullScreen)}
                         type="text"
                         className="text-field"
@@ -51,7 +105,30 @@ const Topbar = () => {
                             }
                         }}
                     />
-                </Box>
+                </Box> */}
+                    <Box sx={{
+                        display: 'inline-block',
+                        height: '60px',
+                        overflow: "auto",
+                        whiteSpace: "nowrap",
+                        }}>
+                        {pillOptions.map(element => {
+                            return(
+                                <Button
+                                variant="solid"
+                                endIcon= {element.icon}
+                                sx={{
+                                    fontSize: '17px',
+                                    margin: '10px 5px',
+                                    backgroundColor: '#808000',
+                                    borderRadius: '20px'
+                                    }}
+                                    >
+                                {element.title}
+                            </Button>
+                            )
+                        })}
+                    </Box>
             </Box>
         </>
     )
